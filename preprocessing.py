e778fb5f1155008caf26a106a3fe60a2eff59471
Yixuan Wei <weason1998@gmail.com>
2020-12-03 23:25:54 +0800

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import torch
+# from torchvision.transforms import functional as F
 
 class Normalize(object):
 
@@ -15,6 +16,8 @@ class Preprocessing(object):
     def __init__(self, framenum=16):
         self.norm = Normalize(mean=[110.6, 103.2, 96.3], std=[1.0, 1.0, 1.0])
         self.framenum = framenum
+        self.mean = [110.6, 103.2, 96.3]
+        self.std = [1.0, 1.0, 1.0]
 
     def _zero_pad(self, tensor, size):
         n = size - len(tensor) % size
@@ -25,8 +28,10 @@ class Preprocessing(object):
             return torch.cat((tensor, z), 0)
 
     def __call__(self, tensor, ):
+        #tensor = self.norm(tensor)
+        #tensor = F.normalize(tensor, mean=self.mean, std=self.std)
+        tensor = tensor / 255
         tensor = self._zero_pad(tensor, self.framenum)
-        tensor = self.norm(tensor)
         tensor = tensor.view(-1, self.framenum, 3, tensor.size(-2), tensor.size(-1))
         tensor = tensor.transpose(1, 2)
         return tensor
